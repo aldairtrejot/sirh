f089d63957be2685dac512cabfa4551083eb1ebc
Joaqs <joaquin.hernandez@imssbienestar.gob.mx>
2024-11-19 22:27:28 -0600

    commit consolidado
    Union
    
    E.CAMPA
    
    ACTUALIZACION DE E.CAMPA
    
    s
    
    correciones ultimas
    
    sd
    
    das
    
    Update FaltaM.php
    
    oiopoi
    
    jhgiugf
    
    hfgd
    
    inge
    
    rwere
    
    gi
    
    jhfjh
    
    sdf
    
    sdfsd
    
    Update PlazasM.php
    
    dsadas

--- App/View/Hraes/Modulo/IncidenciasM/index.php
+++ App/View/Hraes/Modulo/IncidenciasM/index.php
@@ -6,12 +6,7 @@
                 <h6 class="card-header text-center background-modal color-text-tittle">Tipo de incidencia</h6>
                 <div class="nav flex-column nav-pills text-tittle-card-nav-x" id="v-tabs-tab" role="tablist"
                     aria-orientation="vertical">
-                    <a onclick="buscarRetardo();" class="nav-link-mod active" id="v-tabs-home-tab" data-toggle="pill"
-                        href="#v-tabs-home" role="tab" aria-controls="v-tabs-home" aria-selected="true">
-                        <i class="fa fa-folder-open mr-2"></i> Retardos</a>
-                    <a onclick="buscarFalta();" class="nav-link-mod" id="v-tabs-profile-tab" data-toggle="pill"
-                        href="#v-tabs-profile" role="tab" aria-controls="v-tabs-profile" aria-selected="false">
-                        <i class="fa fa-folder-open mr-2"></i> Faltas</a>
+                    
                     <a onclick="buscarLicencia();" class="nav-link-mod" id="v-tabs-messages-tab" data-toggle="pill"
                         href="#v-tabs-messages" role="tab" aria-controls="v-tabs-messages" aria-selected="false">
                         <i class="fa fa-folder-open mr-2"></i> Licencias</a>
@@ -29,16 +24,7 @@
             <div class="tab-content" id="v-tabs-tabContent">
                 <div class="tab-pane fade show active" id="v-tabs-home" role="tabpanel"
                     aria-labelledby="v-tabs-home-tab">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card font-size-modulo shadow-lg">
-                                    <h5 class="card-header text-center background-modal color-text-tittle">Retardo</h5>
-                                    <div class="card-body">
-                                        <?php include 'Retardo/index.php' ?>
-                                    </div>
-                                </div>
-                            </div>
+                    
                         </div>
                     </div>
                 </div>
